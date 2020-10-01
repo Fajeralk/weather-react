cf5fd1e98b920a2a3627c8afac17e3df53d1ad92
Fajeralk <66381900+Fajeralk@users.noreply.github.com>
2020-10-01 14:49:49 -0700

    axios added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import "./App.css";
+// eslint-disable-next-line
+import Search from "./Search";
+// eslint-disable-next-line
+import Day from "./Day";
+// eslint-disable-next-line
+import Forecast from "./Forecast";
+// eslint-disable-next-line
+import Temperature from "./Temperature";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container">
+      <nav className="navbar navbar-light bg-light">
+        <form className="form-inline" id="search-form">
+          <h1>
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search City"
+              aria-label="search"
+              id="query"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              GO!
+            </button>
+            <span className="units" id="current-temp">
+              <button
+                type="button"
+                className="btn btn-outline-info"
+                id="cels"
+                // eslint-disable-next-line
+                className="active"
+              >
+                °C
+              </button>
+              <button type="button" className="btn btn-outline-info" id="fah">
+                °F
+              </button>
+            </span>
+          </h1>
+        </form>
+      </nav>
+      <img
+        src="https://www.thisiscolossal.com/wp-content/uploads/2010/11/four-seasons.jpg"
+        alt="season change"
+        className="illustration"
+        width="1100"
+        height="300"
+      />
+
+      <p className="foot">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Fajeralk/Faje-s-project-plus"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Open source code by
         </a>
-      </header>
+        Fajer alkhubaizi
+      </p>
     </div>
   );
 }
-
-export default App;
---
+++ src/Search.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  const [city, setCity] = useState(null);
+  const [updated, setUpdated] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function showWeather(response) {
+    setUpdated(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+  function handleSearch(event) {
+    event.preventDefault();
+    let unit = "metric";
+    let apiKey = "3845bbf755c7a6b2d8df3dba924feec5";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function updateSearch(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSearch}>
+      <input type="search" placeholder="Type a city" onChange={updateSearch} />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (updated) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature:{Math.round(weather.temperature)} °C </li>
+          <li>Descri[tion:{weather.description} </li>
+          <li>Humidity:{weather.humidity} % </li>
+          <li>Wind:{weather.wind} km/h </li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
